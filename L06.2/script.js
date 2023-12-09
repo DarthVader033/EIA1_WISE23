baf4a1f2aaa1b05b96f14914adb4a79d0a93cd3a
DarthVader033 <alitamaier@gmail.com>
2023-12-09 18:20:25 +0100

    l06.2

---
+++ L06.2/script.js
@@ -0,0 +1,36 @@
+
+
+// index 0 1 2 3 4 
+let myArray = [
+    "You are stranded on the Death Star surrounded by imperial personell. You must escape and find your rebel friends. Are you choosing the left or right hallway?",
+    "Darth Vader approaches you with his lightsaber drawn. Do you surrender or fight?",
+    "Stormtroopers put you in binders and are leading you towards a prison cell. Do your surrender or fight?",
+    "You die",
+]
+
+let yourChoice = prompt (myArray[0])
+
+if (yourChoice == "left") {
+   let myAction = prompt (myArray[1]);
+
+    if (yourChoice == "surrender") {
+        let myAction = prompt (myArray[3]);
+    }
+    else if ("fight") {
+        let myAction = prompt (myArray[3]);
+    }
+    
+} 
+
+else if (yourChoice == "right") {
+   let myAction = prompt (myArray[2]);
+    if (yourChoice =="surrender") {
+        let myAction = prompt (myArray[3]);
+    }
+    else if ("fight"){
+        let myAction = prompt (myArray[3]);
+    }
+
+}
+
+
--- L06/script.js
+++ L06/script.js
@@ -9,7 +9,7 @@ if (yourChoice == "left") {
    let myAction = prompt ("Darth Vader approaches you with his lightsaber drawn. Do you surrender or fight?");
 
     if (yourChoice == "surrender") {
-        let myAction = prompt ("you are put in a prison cell and are now sceduled for execution. Your only hope are the Rebels.")
+        let myAction = prompt ("You are put in a prison cell and are now sceduled for execution. Your only hope are the Rebels.")
     }
     else if ("fight") {
         let myAction = prompt ("You die.")
@@ -20,7 +20,7 @@ if (yourChoice == "left") {
 else if (yourChoice == "right") {
    let myAction = prompt ("Stormtroopers put you in binders and are leading you towards a prison cell. Do you surrender or fight? ");
     if (yourChoice =="surrender") {
-        let myAction = prompt ("Your are put in a prison cell and are now sceduled for execution. Your only hope are the rebels.")
+        let myAction = prompt ("You are put in a prison cell and are now sceduled for execution. Your only hope are the rebels.")
     }
     else if ("fight"){
         let myAction = prompt ("You die.")
